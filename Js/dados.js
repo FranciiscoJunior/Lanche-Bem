2bb8660fb1fce25fe5a9ddbcb6a3dcfe565743c9
Francisco <franciscoamj1001@gmail.com>
2024-07-24 17:24:48 -0300

    .

---
+++ Js/dados.js
@@ -0,0 +1,491 @@
+var MENU = {
+    "acai": [
+        {
+            "id": "001",
+            "img": "/img/cardapio/Acai/AÇAÍ COMPLETO.avif",
+            "name": "AÇAÍ NA BARCA",
+            "desc": "Açai na barca completo, sabores",
+            "price": 41,99
+        },
+
+        {
+            "id": "002",
+            "img": "/img/cardapio/Acai/AÇAÍ NA BARCA.avif",
+            "name": "AÇAÍ SIMPLES 350G",
+            "desc": "Pote de açai simples 350 G",
+            "price": 17,99
+        },
+
+        {
+            "id": "003",
+            "img": "/img/cardapio/Acai/AÇAÍ NO COPO 300 ML.avif",
+            "name": "AÇAÍ NO COPO 300 ML",
+            "desc": "Açai no copo diversos sabores e tamanhos",
+            "price": 19,90
+        },
+
+        {
+            "id": "004",
+            "img": "/img/cardapio/Acai/AÇAÍ SIMPLES 350G.avif",
+            "name": "AÇAÍ COMPLETO MISTO",
+            "desc": "Sabores variados de diferentes tamanhos e tipos",
+            "price": 31,90
+        },
+
+        {
+            "id": "005",
+            "img": "/img/cardapio/Acai/MARMITA DE AÇAI ( P ).avif",
+            "name": "MARMITA DE AÇAI ( P )",
+            "desc": "Desfrute de uma generosa Marmita de Açaí (P), pertencente à nossa refinada categoria de Açaí. Uma combinação perfeitamente balanceada e irresistível, agraciada com três recheios cuidadosamente selecionados para proporcionar uma experiência gastronômica única. Cada ingrediente foi escolhido a dedo para realçar o sabor autêntico e a rica textura do açaí. Uma delícia tropical que promete encantar o seu paladar e enriquecer a sua alimentação. Não perca a oportunidade de saborear esta maravilha!",
+            "price": 25,90
+        },
+
+        {
+            "id": "006",
+            "img": "/img/cardapio/Acai/AÇAI NA MARMITA ( M ).avif",
+            "name": "AÇAI NA MARMITA ( M )",
+            "desc": "Descubra a experiência revigorante de saborear nosso 'Açaí na Marmita (M)'. Esse prato, pertencente à categoria de açaí, é uma combinação perfeita de sabor e saúde. Oferecemos uma seleção de quatro recheios deliciosos e nutritivos que complementam de maneira espetacular a riqueza do nosso açaí. Cada mordida é uma explosão de sabor que garante satisfazer o seu paladar. Este é, sem dúvida, o prato que você procura para um lanche saudável e delicioso.",
+            "price": 31,90
+        },
+
+        {
+            "id": "007",
+            "img": "/img/cardapio/Acai/AÇAI NA MARMITA ( G ).avif",
+            "name": "AÇAI NA MARMITA ( G )",
+            "desc": "Descubra a deliciosa experiência de saborear o nosso Açaí na Marmita (G). Este magnífico prato da categoria Açaí é generosamente preenchido com cinco recheios distintos e cuidadosamente selecionados para proporcionar um deleite para o seu paladar. Cada mordida é uma explosão de sabor tropical que combina perfeitamente com a doçura do açaí. Uma combinação irresistível que irá surpreendê-lo a cada colherada. Perfeito para refrescar o seu Dia!",
+            "price": 37,99
+        },
+
+        {
+            "id": "008",
+            "img": "/img/cardapio/Acai/AÇAI NA MARMITA ( PARA A FAMILIA TODA ).avif",
+            "name": "AÇAI NA MARMITA ( PARA A FAMILIA TODA )",
+            "desc": "Delicie-se com o nosso 'Açaí na Marmita (para a família toda)', uma oferta repleta de sabor da categoria 'Açaí'. Envolvendo uma rica combinação de sete recheios cuidadosamente selecionados, este prato é uma verdadeira explosão de sabores tropicais. Cada ingrediente foi escolhido com o objetivo de proporcionar uma experiência culinária única e inesquecível. Desfrute deste deleite nutritivo com a família e permita-se mergulhar num mar de prazer gustativo. Ideal para compartilhar em momentos especiais ou simplesmente para satisfazer aquela vontade de algo doce e refrescante.",
+            "price": 44,90
+        },
+
+        {
+            "id": "009",
+            "img": "/img/cardapio/Acai/AÇAÍ NO COPO 300 ML.avif",
+            "name": "AÇAI NO POTE PURO 350 ML",
+            "desc": "AÇAI NO POTE PURO 350 ML",
+            "price": 30,90
+        },
+
+        {
+            "id": "010",
+            "img": "/img/cardapio/Acai/AÇAI TRUFADO 550 ML.avif",
+            "name": "AÇAI TRUFADO 550 ML",
+            "desc": "Saboreie nosso delicioso açaí de 500ml com creme de avelã, morangos frescos, leite em pó, leite condensado e dois irresistíveis brigadeiros para adoçar ainda mais o seu Dia! Temos mais de 15 opções de adicionais.",
+            "price": 29,90
+        },
+
+        {
+            "id": "011",
+            "img": "/img/cardapio/Acai/BARCA DE AÇAÍ PEQUENA.avif",
+            "name": "BARCA DE AÇAÍ PEQUENA",
+            "desc": "Nossa deliciosa barca de açaí pequena com 350 ml de açai + adicionais de topping a gosto com no mínimo 3 acompanhamentos! Como Resistir a primeira colherada? Hum.. Deu até agua na boca",
+            "price": 31,00
+        },
+
+        {
+            "id": "012",
+            "img": "/img/cardapio/Acai/COPO DE AÇAI 500ML COM 3 ACOMPANHAMENTOS GRATIS!!!.avif",
+            "name": "COPO DE AÇAI 500ML COM 3 ACOMPANHAMENTOS GRATIS!!!",
+            "desc": "A Frutaça e seu purissimo copo de açaí 500 ml com até 3 acompanhamentos a sua escolha!!",
+            "price": 27,40
+        },
+    ],
+
+    "bebidas": [
+        {
+            "id": "001",
+            "img": "/img/cardapio/Bebidas/AGUA MINERAL LINDOYA 510ML SENSE.avif",
+            "name": "AGUA MINERAL LINDOYA 510ML SENSE S/GAS",
+            "desc": "",
+            "price": 2,99
+        },
+
+        {
+            "id": "002",
+            "img": "/img/cardapio/Bebidas/ÁGUA MINERAL COM GÁS MINALBA 510ML.avif",
+            "name": "ÁGUA MINERAL COM GÁS MINALBA 510ML",
+            "desc": "",
+            "price": 4,50
+        },
+
+        {
+            "id": "003",
+            "img": "/img/cardapio/Bebidas/AGUA MINERAL SEM GÁS CRYSTAL 1,5L.avif",
+            "name": "AGUA MINERAL SEM GÁS CRYSTAL 1,5L",
+            "desc": "",
+            "price": 6,59
+        },
+
+        {
+            "id": "004",
+            "img": "/img/cardapio/Bebidas/AGUA MINERAL SEM GÁS MINALBA 1,5L.avif",
+            "name": "AGUA MINERAL SEM GÁS MINALBA 1,5L",
+            "desc": "",
+            "price": 6,39
+        },
+
+        {
+            "id": "005",
+            "img": "/img/cardapio/Bebidas/ÁGUA TÔNICA ANTARCTICA 1L.avif",
+            "name": "ÁGUA TÔNICA ANTARCTICA 1L",
+            "desc": "",
+            "price": 15,95
+        },
+
+        {
+            "id": "006",
+            "img": "/img/cardapio/Bebidas/ÁGUA TÔNICA ANTARCTICA 350ML.avif",
+            "name": "ÁGUA TÔNICA ANTARCTICA 350ML",
+            "desc": "",
+            "price": 6,50
+        },
+
+        {
+            "id": "007",
+            "img": "/img/cardapio/Bebidas/ÁGUA DE COCO COCO QUADRADO 1L.avif",
+            "name": "ÁGUA DE COCO COCO QUADRADO 1L",
+            "desc": "",
+            "price": 14,99
+        },
+
+        {
+            "id": "008",
+            "img": "/img/cardapio/Bebidas/SCHWEPPES CITRUS 350ML.avif",
+            "name": "SCHWEPPES CITRUS 350ML",
+            "desc": "",
+            "price": 5,90
+        },
+
+        {
+            "id": "009",
+            "img": "/img/cardapio/Bebidas/COCA-COLA ZERO 350ML.avif",
+            "name": "COCA-COLA ZERO 350ML",
+            "desc": "",
+            "price": 6,39
+        },
+
+        {
+            "id": "010",
+            "img": "/img/cardapio/Bebidas/FANTA LARANJA 350ML.avif",
+            "name": "FANTA LARANJA 350ML",
+            "desc": "",
+            "price": 5,99
+        },
+
+        {
+            "id": "011",
+            "img": "/img/cardapio/Bebidas/FANTA UVA 350ML.avif",
+            "name": "FANTA UVA 350ML",
+            "desc": "",
+            "price": 5,99
+        },
+
+        {
+            "id": "012",
+            "img": "/img/cardapio/Bebidas/REFRIGERANTE GUARANÁ ANTARCTICA LATA 350ML.avif",
+            "name": "REFRIGERANTE GUARANÁ ANTARCTICA LATA 350ML",
+            "desc": "",
+            "price": 6,39
+        },
+    ],
+
+    "bolinho": [
+        {
+            "id": "001",
+            "img": "/img/cardapio/Bolinho/BROWNIE ORIGINAL.avif",
+            "name": "BROWNIE ORIGINAL",
+            "desc": "",
+            "price": 11,99
+        },
+
+        {
+            "id": "002",
+            "img": "/img/cardapio/Bolinho/BROWNIE 3 CHOCOLATES.avif",
+            "name": "BROWNIE 3 CHOCOLATES",
+            "desc": "",
+            "price": 11,99
+        },
+
+        {
+            "id": "003",
+            "img": "/img/cardapio/Bolinho/BROWNIE COM CHOCOLATE MEIO AMARGO.avif",
+            "name": "BROWNIE COM CHOCOLATE MEIO AMARGO",
+            "desc": "",
+            "price": 11,99
+        },
+
+        {
+            "id": "004",
+            "img": "/img/cardapio/Bolinho/BROWNIE COM CHOCOLATE BRANCO.avif",
+            "name": "BROWNIE COM CHOCOLATE BRANCO",
+            "desc": "",
+            "price": 11,99
+        },
+
+        {
+            "id": "005",
+            "img": "/img/cardapio/Bolinho/BROWNIE ORIGINAL.avif",
+            "name": "BROWNIE COM BRIGADEIRO",
+            "desc": "",
+            "price": 11,99
+        },
+
+        {
+            "id": "006",
+            "img": "/img/cardapio/Bolinho/BROWNIE COM BRIGADEIRO DE NINHO.avif",
+            "name": "BROWNIE COM BRIGADEIRO DE NINHO",
+            "desc": "",
+            "price": 12,99
+        },
+
+        {
+            "id": "007",
+            "img": "/img/cardapio/Bolinho/BROWNIE COM DOCE DE LEITE.avif",
+            "name": "BROWNIE COM DOCE DE LEITE",
+            "desc": "",
+            "price": 11,99
+        },
+
+        {
+            "id": "008",
+            "img": "/img/cardapio/Bolinho/BROWNIE COM NUTELLA.avif",
+            "name": "BROWNIE COM NUTELLA",
+            "desc": "",
+            "price": 12,99
+        },
+
+        {
+            "id": "009",
+            "img": "/img/cardapio/Bolinho/BROWNIE CACAU.avif",
+            "name": "BROWNIE CACAU",
+            "desc": "",
+            "price": 11,99
+        },
+
+        {
+            "id": "010",
+            "img": "/img/cardapio/Bolinho/MARMITINHA COM BRIGADEIRO DE COLHER AO LEITE.avif",
+            "name": "MARMITINHA COM BRIGADEIRO DE COLHER AO LEITE",
+            "desc": "",
+            "price": 25,99
+        },
+
+        {
+            "id": "011",
+            "img": "/img/cardapio/Bolinho/MARMITINHA MIX.avif",
+            "name": "MARMITINHA MIX",
+            "desc": "",
+            "price": 25,99
+        },
+
+        {
+            "id": "012",
+            "img": "/img/cardapio/Bolinho/MARMITINHA MIX.avif",
+            "name": "MARMITINHA ORIGINAL COM NUTELLA E KIT KAT AO LEITE",
+            "desc": "",
+            "price": 25,99
+        },
+    ],
+
+    "sobremesa": [
+        {
+            "id": "001",
+            "img": "/img/cardapio/Sobremesa/PUDIM DE CHOCOLATE BELGA 70% INDIVIDUAL (150G).avif",
+            "name": "PUDIM DE CHOCOLATE BELGA 70% INDIVIDUAL (150G)",
+            "desc": "",
+            "price": 17,90
+        },
+
+        {
+            "id": "002",
+            "img": "/img/cardapio/Sobremesa/PUDIM DE PISTACHE INDIVIDUAL (150G).avif",
+            "name": "PUDIM DE PISTACHE INDIVIDUAL (150G)",
+            "desc": "",
+            "price": 17,90
+        },
+
+        {
+            "id": "003",
+            "img": "/img/cardapio/Sobremesa/PUDIM DE DOCE DE LEITE HAVANNA INDIVIDUAL (150G).avif",
+            "name": "PUDIM DE DOCE DE LEITE HAVANNA INDIVIDUAL (150G)",
+            "desc": "",
+            "price": 17,90
+        },
+
+        {
+            "id": "004",
+            "img": "/img/cardapio/Sobremesa/PUDIM DE FAVA DE BAUNILHA INDIVIDUAL (150G).avif",,
+            "name": "PUDIM DE FAVA DE BAUNILHA INDIVIDUAL (150G)",
+            "desc": "",
+            "price": 17,90
+        },
+
+        {
+            "id": "005",
+            "img": "/img/cardapio/Sobremesa/PUDIM DE CHOCOLATE BRANCO LAKA INDIVIDUAL (150G).avif",
+            "name": "PUDIM DE CHOCOLATE BRANCO LAKA INDIVIDUAL (150G)",
+            "desc": "",
+            "price": 15,99
+        },
+
+        {
+            "id": "006",
+            "img": "/img/cardapio/Sobremesa/PUDIM DE NUTELLA INDIVIDUAL (150G).avif",
+            "name": "PUDIM DE NUTELLA INDIVIDUAL (150G)",
+            "desc": "",
+            "price": 15,90
+        },
+
+        {
+            "id": "007",
+            "img": "/img/cardapio/Sobremesa/PUDIM DE NINHO COM NUTELLA INDIVIDUAL (150G).avif",
+            "name": "PUDIM DE NINHO COM NUTELLA INDIVIDUAL (150G)",
+            "desc": "",
+            "price": 15,90
+        },
+
+        {
+            "id": "008",
+            "img": "/img/cardapio/Sobremesa/PUDIM DE CHURROS INDIVIDUAL (150G).avif",
+            "name": "PUDIM DE CHURROS INDIVIDUAL (150G)",
+            "desc": "",
+            "price": 15,90
+        },
+
+        {
+            "id": "009",
+            "img": "/img/cardapio/Sobremesa/CAIXA P2 PUDINS.avif",
+            "name": "CAIXA P/ 2 PUDINS",
+            "desc": "",
+            "price": 31,80
+        },
+
+        {
+            "id": "010",
+            "img": "/img/cardapio/Sobremesa/CALDA FRUTAS VERMELHAS.avif",
+            "name": "CALDA FRUTAS VERMELHA",
+            "desc": "Deliciosa calda de frutas vermelhas para acompanhar nossos pudins! São 60ml de calda que cobre 1 pudim individuais!",
+            "price": 6,90
+        },
+
+        {
+            "id": "011",
+            "img": "/img/cardapio/Sobremesa/BRIGADEIRO NO POTE - CHOCOLATE BELGA.avif",
+            "name": "BRIGADEIRO NO POTE - CHOCOLATE BELGA",
+            "desc": "Toda sofisticação do chocolate belga Callebaut na sobremesa favorita dos brasileiros: o brigadeiro! Nessa surpreendente versão cremosa, você encontrará uma textura aveludada que eleva a sensação de prazer que a sobremesa já oferece, além da praticidade do nosso generoso pote de 170 gramas.",
+            "price": 18,90
+        },
+
+        {
+            "id": "012",
+            "img": "/img/cardapio/Sobremesa/BRIGADEIRO NO POTE - DOIS AMORES.avif",
+            "name": "BRIGADEIRO NO POTE - DOIS AMORES",
+            "desc": "A combinanção entre suavidade e intensidade, trouxemos na versão cremosa a combinação do brigadeiro branco com o brigadeiro tradicional. Pote de 170 gramas.",
+            "price": 18,90
+        },
+    ],
+
+    "pizza": [
+        {
+            "id":
+            "img":
+            "name":
+            "desc":
+            "price":
+        },
+
+        {
+            "id": 
+            "img":
+            "name":
+            "desc":
+            "price":
+        },
+
+        {
+            "id": 
+            "img":
+            "name":
+            "desc":
+            "price":
+        },
+
+        {
+            "id": 
+            "img":
+            "name":
+            "desc":
+            "price":
+        },
+
+        {
+            "id": 
+            "img":
+            "name":
+            "desc":
+            "price":
+        },
+
+        {
+            "id": 
+            "img":
+            "name":
+            "desc":
+            "price":
+        },
+
+        {
+            "id": 
+            "img":
+            "name":
+            "desc":
+            "price":
+        },
+
+        {
+            "id": 
+            "img":
+            "name":
+            "desc":
+            "price":
+        },
+
+        {
+            "id": 
+            "img":
+            "name":
+            "desc":
+            "price":
+        },
+
+        {
+            "id": 
+            "img":
+            "name":
+            "desc":
+            "price":
+        },
+
+        {
+            "id": 
+            "img":
+            "name":
+            "desc":
+            "price":
+        },
+
+        {
+            "id": 
+            "img":
+            "name":
+            "desc":
+            "price":
+        },
+    ]
+}
